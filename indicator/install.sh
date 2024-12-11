c1b506eac31c0f510e40c450cf0c93d4057ba5d0
Pavlo Rudy <paulcarroty@users.noreply.github.com>
2024-12-11 20:38:45 +0200

    fix hardcoded_paths warning

--- indicator/install.sh
+++ indicator/install.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p /home/phablet/.config/upstart/
 mkdir -p /home/phablet/.local/share/unity/indicators/
 
-cp -v /opt/click.ubuntu.com/indicator.upower.ernesst/current/indicator/ernesst-indicator-upower.conf /home/phablet/.config/upstart/
-cp -v /opt/click.ubuntu.com/indicator.upower.ernesst/current/indicator/com.ernesst.indicator.upower /home/phablet/.local/share/unity/indicators/
+cp -v $(dirname "${BASH_SOURCE[0]}")/indicator.upower.ernesst/current/indicator/ernesst-indicator-upower.conf /home/phablet/.config/upstart/
+cp -v $(dirname "${BASH_SOURCE[0]}")/indicator.upower.ernesst/current/indicator/com.ernesst.indicator.upower /home/phablet/.local/share/unity/indicators/
 
 echo "indicator-upower installed!"
