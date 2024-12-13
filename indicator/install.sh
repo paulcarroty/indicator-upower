e129c3481d6755143507cd5fed7801f7b63046f8
Pavlo Rudy <paulcarroty@users.noreply.github.com>
2024-12-13 21:53:33 +0200

    fix install.sh

--- indicator/install.sh
+++ indicator/install.sh
@@ -9,8 +9,8 @@ mkdir -p /home/phablet/.local/share/unity/indicators/
 # mkdir -p /home/phablet/.local/share/ayatana/indicators/
 
 
-cp -v $(dirname "${BASH_SOURCE[0]}")/indicator.upower.ernesst/current/indicator/indicator-upower.service /home/phablet/.config/systemd/user/
-cp -v $(dirname "${BASH_SOURCE[0]}")/indicator.upower.ernesst/current/indicator/upower.indicator /home/phablet/.local/share/unity/indicators/
+cp -v $(dirname "${BASH_SOURCE[0]}")/indicator-upower.service /home/phablet/.config/systemd/user/
+cp -v $(dirname "${BASH_SOURCE[0]}")/upower.indicator /home/phablet/.local/share/unity/indicators/
 
 # /home/phablet/.local/share/ayatana/indicators/
 
