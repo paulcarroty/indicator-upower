77daa4385ba99b49ad09bbf0655aa85b410f2a65
Pavlo Rudy <paulcarroty@users.noreply.github.com>
2024-12-13 23:47:51 +0200

    fix indicator paths

--- indicator/install.sh
+++ indicator/install.sh
@@ -6,8 +6,8 @@ mkdir -p /home/phablet/.config/systemd/user
 mkdir -p /home/phablet/.local/share/ayatana/indicators/
 
 
-cp -v $(dirname "${BASH_SOURCE[0]}")/indicator.upower.ernesst.fork/current/indicator/indicator-upower.service /home/phablet/.config/systemd/user/
-cp -v $(dirname "${BASH_SOURCE[0]}")/indicator.upower.ernesst.fork/current/indicator/upower.indicator /home/phablet/.local/share/ayatana/indicators/
+cp -v /opt/click.ubuntu.com/indicator.upower.ernesst.fork/current/indicator/indicator-upower.service /home/phablet/.config/systemd/user/
+cp -v /opt/click.ubuntu.com/indicator.upower.ernesst.fork/current/indicator/upower.indicator /home/phablet/.local/share/ayatana/indicators/
 
 
 systemctl --user enable indicator-upower.service
